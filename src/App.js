76d25faef29d09f2aad4ce9e6a5cb4022b5a2bc0
leilaashur <leilahashur@gmail.com>
2023-07-06 10:37:41 +0300

    hook commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
+import Products from './Products';
+import Navbar from './Navbar';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div>
+    <Navbar/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/home'element={<Products />}/>
+      <Route path='/home'element={<Products />}/>
+      <Route path='/about'element={<Products />}/>
+      
+    </Routes>
+    </BrowserRouter>
+   </div>
+);
 }
+   
+ 
 
 export default App;
---
+++ src/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './style.css'
+const Navbar=()=>{
+    return(
+        <navbar>
+        <ul>
+            <li><a href = '/Home'></a>Home</li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/login">Login</a></li>
+
+        </ul>
+        </navbar>
+    )
+}
+export default Navbar
